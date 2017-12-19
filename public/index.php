a26b03185e2a2a80c249e0e832d4e26753cdedd3
Igor Baskakov <igor.baskak@gmail.com>
2017-12-19 17:41:04 +0300

    add config

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+$config = include __DIR__ . '/../protected/config.php';
 
-$obj = new \T4\Core\Std();
-$obj->foo = 42;
-echo $obj['foo'];
+echo 'Добро пожаловать на ' . $config['domain'];
+//$obj = new \T4\Core\Std();
+//$obj->foo = 42;
+//echo $obj['foo'];
