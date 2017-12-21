6f7c30e787b11669bb8e6504fb7d265cc73692b4
Igor Baskakov <igor.baskak@gmail.com>
2017-12-21 23:49:55 +0300

    add info about project

--- public/index.php
+++ public/index.php
@@ -4,6 +4,3 @@ require __DIR__ . '/../vendor/autoload.php';
 $config = include __DIR__ . '/../protected/config.php';
 
 echo 'Добро пожаловать на ' . $config['domain'];
-//$obj = new \T4\Core\Std();
-//$obj->foo = 42;
-//echo $obj['foo'];
